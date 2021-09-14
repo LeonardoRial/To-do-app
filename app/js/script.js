832d5ed52acca96fc6b7d09031eb808c2687a3ae
LeonardoCamposRial <73833933+LeonardoCamposRial@users.noreply.github.com>
2021-09-14 12:45:12 +0100

    todo

--- app/js/script.js
+++ app/js/script.js
@@ -3,14 +3,16 @@ const button = document.querySelector(".todo-button");
 const list = document.querySelector(".todo-list");
 
 //Event Listners
-
+list.addEventListener("click", deleteCheck);
 button.addEventListener("click", addTodo);
 //Functions
+
 function addTodo(event) {
   //prevent reload of page
   event.preventDefault();
 
   //create elements
+
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
   const newTodo = document.createElement("li");
@@ -18,6 +20,7 @@ function addTodo(event) {
   todoDiv.appendChild(newTodo);
 
   //create buttons
+
   const completed = document.createElement("button");
   completed.innerHTML = '<i class="fas fa-check"></i>';
   completed.classList.add("completed-btn");
@@ -29,5 +32,26 @@ function addTodo(event) {
   todoDiv.appendChild(trash);
 
   //append todos to the list
+
   list.appendChild(todoDiv);
+
+  //Clear input value
+  input.value = "";
+}
+
+function deleteCheck(event) {
+  const target = event.target;
+  const todo = target.parentElement;
+  //delete
+  if (target.classList[0] === "trash-btn") {
+    todo.classList.add("dissapear");
+    todo.addEventListener("transitionend", () => {
+      todo.remove();
+    });
+  }
+  //completed
+  if (target.classList[0] === "completed-btn") {
+    todo.classList.toggle("done");
+    target.classList.toggle("done-btn");
+  }
 }
